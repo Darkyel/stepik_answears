47a849f58682b86418a45b69e127e0106c53db65
Igor <igorsas98.1998@gmail.com>
2021-11-16 22:23:32 +0200

    Create 2.3.js

---
+++ 1. JavaScript для начинающих/2.3.js
@@ -0,0 +1,25 @@
+/step/3
+
+function testStr(a, b) {
+    return a.length + b.length;
+}
+
+/step/5
+
+function testStr(str, n) {
+return str.charAt(n-1)
+}
+
+/step/8
+
+function testStr(a, b) {
+    a=a.toUpperCase();
+    b=b.toLowerCase();
+    return a.concat( b);
+}
+
+/step/12
+
+function testStr(a, b) {
+    return a.indexOf(b,0)
+}
