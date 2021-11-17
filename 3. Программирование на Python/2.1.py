5c9efb96c541a40d53f00c63c2f53a9f27d2f6d2
Igor <igorsas98.1998@gmail.com>
2021-11-17 08:52:58 +0200

    Update 2.1.py

--- 3. Программирование на Python/2.1.py
+++ 3. Программирование на Python/2.1.py
@@ -1,1 +1,34 @@
+/step/4
 
+13
+
+/step/5
+
+9
+
+/step/9
+
+17
+
+/step/11
+
+numbers = []
+while True:
+    x = int(input())
+    numbers.append(x)
+    if x == 0:
+        break
+print(sum(numbers))
+
+
+/step/12
+
+def lcm(a, b):
+    c = a * b
+    result = c
+    while c > 0:
+        if c % a == 0 and c % b == 0:
+            result = c
+        c -= 1
+    return result
+print(lcm(int(input()), int(input())))
