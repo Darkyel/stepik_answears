ad2c1136120ca7684afec219de0d4a2af0c39838
Igor <igorsas98.1998@gmail.com>
2022-01-20 10:47:00 +0200

    Create 2.3.py

---
+++ 5. "Поколение Python": курс для начинающих/2.3.py
@@ -0,0 +1,53 @@
+/step/2
+
+31-12-2019
+
+/step/3
+
+Mercury*Venus!Mars**Jupiter?
+
+/step/4
+
+5
+
+/step/5
+
+print("I", "like", "Python", sep="***")
+
+/step/6
+
+na = input()
+z = input()
+x = input()
+c = input()
+print(z, x, c, sep=na)
+
+/step/7
+
+ni = input()
+print("Привет, " + ni, end="!")
+
+/step/9
+
+teacher_2
+teacher
+teacher2
+
+/step/10
+
+Pascal
+
+/step/11
+
+Python
+
+/step/13
+
+print('Follow PEP8!')
+print('Follow', 'PEP8!')
+print('Follow', 'PEP8', sep='**')
+name = input()
+
+/step/14
+
+Python+C#=awesome
